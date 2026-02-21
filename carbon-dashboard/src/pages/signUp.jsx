36d60df3867874dfc7f2ec81a8cff6a0933f7dac
Kishor M <kishorachuzz1104@gmail.com>
2026-02-21 21:17:07 +0530

    signUp

--- carbon-dashboard/src/pages/signUp.jsx
+++ carbon-dashboard/src/pages/signUp.jsx
@@ -2,10 +2,10 @@ import { Eye, EyeOff } from 'lucide-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import bgImage from './bg.png';
-const Login = ({ setIsLoggedIn }) => {
+const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [username, seUsername] = useState('');
+  const [username, setUsername] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [show, setShow] = useState(false);
   
@@ -13,28 +13,25 @@ const Login = ({ setIsLoggedIn }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsLoading(true);
 
     // Simulate API call/loading effect like your friend set up
     setTimeout(() => {
       setIsLoading(false);
       
       // 1. Extract the name from the email (vibin@example.com -> vibin)
-      const extractedName = email.split('@')[0];
+      const Name = username;
       
       // 2. Save it to storage so the app remembers them
-      localStorage.setItem('carbonUser', extractedName);
+      localStorage.setItem('carbonUser', Name);
       
-      // 3. Update global state and teleport to the dashboard
-      setIsLoggedIn(true);
-      navigate('/');
+      navigate('/login');
     }, 1500);
   };
 
   return (
     <div className="flex-1 w-full flex bg-cover bg-center items-center justify-center px-4"
   style={{ backgroundImage: `url(${bgImage})` }}
->
+    >
       <div className="w-full max-w-md rounded-3xl bg-black p-8 shadow-lg border border-gray-800">
         <div className="mb-8 text-center">
           <h1 className="text-3xl font-bold text-white">Register</h1>
@@ -51,7 +48,7 @@ const Login = ({ setIsLoggedIn }) => {
               id="username"
               className="w-full rounded-lg border border-gray-600 bg-gray-900 px-4 py-2 text-white focus:border-green-500 focus:outline-none focus:ring-2 focus:ring-green-500/50"
               placeholder="username"
-              value={email}
+              value={username}
               onChange={(e) => setUsername(e.target.value)}
               required
             />
@@ -99,7 +96,7 @@ const Login = ({ setIsLoggedIn }) => {
             disabled={isLoading}
             className="w-full rounded-lg bg-green-600 px-4 py-2 font-bold text-white transition-colors duration-200 hover:bg-green-500 focus:outline-none focus:ring-4 focus:ring-green-900"
           >
-            {isLoading ? 'Signing In...' : 'Sign In'}
+            Sign UP
           </button>
         </form>
 
@@ -120,4 +117,4 @@ const Login = ({ setIsLoggedIn }) => {
   );
 };
 
-export default Login;
+export default SignUp;
